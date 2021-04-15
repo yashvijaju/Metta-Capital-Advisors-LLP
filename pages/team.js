4e36a0d9a717a77fc86a5d429dc7ac9849de5816
yjaju <yjaju@usc.edu>
2021-04-15 10:25:43 +0530

    Ashish Chovatia

--- pages/team.js
+++ pages/team.js
@@ -20,7 +20,7 @@ const styleHeadline = {color: orange, fontFamily: font}
 export default function Team() {
     const [openPJ, setOpenPJ] = useState(false);
     const [openSS, setOpenSS] = useState(false);
-    const [openSB, setOpenSB] = useState(false);
+    const [openAC, setOpenAC] = useState(false);
 
     const handleOpenPJ = () => {
         setOpenPJ(true);
@@ -38,12 +38,12 @@ export default function Team() {
         setOpenSS(false);
     };
 
-    const handleOpenSB = () => {
-        setOpenSB(true);
+    const handleOpenAC = () => {
+        setOpenAC(true);
       };
     
-    const handleCloseSB = () => {
-        setOpenSB(false);
+    const handleCloseAC = () => {
+        setOpenAC(false);
     };
 
     
@@ -149,6 +149,50 @@ export default function Team() {
                                             
                                         </Typography>
                                         <a href="https://www.linkedin.com/in/saurabh-singhi-ba42b111/" target="_blank"  rel="noopener noreferrer" title="LinkedIn" style={{color: grey, 'paddingLeft': '5px', 'paddingTop': '5px'}}>
+                                            <LinkedIn fontSize="small"/>
+                                        </a>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Hidden>
+                    <Hidden smUp>
+                        <Grid item xs={2}/>
+                        <Grid item xs={1}/>
+                    </Hidden>
+                    <Hidden only="xs">
+                        <Grid item xs={9} sm={5} md={3} onClick={handleOpenAC} style={styleItemwCursor}>
+                            <img src="/assets/ashish.png" alt="Ashish Chovatia" style={{width: '100%', height: 'auto'}}/>
+                            <Grid container spacing={0} direction="row">
+                                <Grid item width="90%">
+                                    <Grid container direction="column">
+                                        <Typography variant="subtitle1" style={styleHeadline}>
+                                            Ashish Chovatia
+                                        </Typography>
+                                        <Typography variant="caption" style={{color: grey, fontFamily: font}}>
+                                            
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                                <Grid item width="10%" style={{'paddingTop': '5px', color: grey}}>
+                                    <LinkedIn fontSize="small"/>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Hidden>
+                    <Hidden smUp>
+                        <Grid item xs={9} sm={5} md={3} style={styleItem}>
+                            <img src="/assets/ashish.png" alt="Ashish Chovatia" style={{width: '100%', height: 'auto'}}/>
+                            <Grid item width="100%">
+                                <Grid container direction="column">
+                                    <Typography variant="subtitle1" style={styleHeadline}>
+                                        Ashish Chovatia
+                                    </Typography>
+                                    <Grid item style={{'paddingTop': '5px', display: 'flex', flexDirection: 'row'}}>
+                                        <Typography variant="caption" style={{fontFamily: font}}>
+                                            
+                                        </Typography>
+                                        <a href="https://www.linkedin.com/in/ashish-chovatia-2038772/" target="_blank"  rel="noopener noreferrer" title="LinkedIn" style={{color: grey, 'paddingLeft': '5px', 'paddingTop': '5px'}}>
                                             <LinkedIn fontSize="small"/>
                                         </a>
                                     </Grid>
@@ -307,6 +351,85 @@ export default function Team() {
                             </Hidden>
                                 
                             
+                            <Grid item xs={1}/>
+                        </Grid>
+                    </div>
+                    </Fade>
+                </Modal>
+                <Modal
+                    style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}
+                    aria-labelledby="image-modal"
+                    aria-describedby="story-modal"
+                    open={openAC}
+                    onClose={handleCloseAC}
+                    closeAfterTransition={true}
+                    BackdropComponent={Backdrop}
+                    BackdropProps={{
+                    timeout: 500,
+                    }}
+                >
+                    <Fade in={openSS}>
+                    <div style={{backgroundColor: 'white', border: '3px solid #f8b04b', boxShadow: 'shadows[5]', width: '60vw', 'maxHeight': '75vh', 'overflowY': 'auto', display: 'flex', flexDirection: 'column', alignItems: 'center', outline: 'none'}}>
+                        <Grid container style={{'padding': '10px'}}>
+                            
+                            <Hidden mdUp>
+                                <Grid item xs={12}>
+                                    <Grid container spacing={0} direction="row">
+                                        <Grid item width="90%">
+                                            <Grid container direction="column">
+                                                <Typography variant="h6" style={styleHeadline}>
+                                                Ashish Chovatia
+                                                </Typography>
+                                                <Typography variant="subtitle1">
+                                                
+                                                </Typography>
+                                            </Grid>
+                                        </Grid>
+                                        <Grid item width="10%" style={{'paddingTop': '5px', 'paddingLeft': '5px'}}>
+                                            <a href="https://www.linkedin.com/in/ashish-chovatia-2038772/" target="_blank"  rel="noopener noreferrer" title="LinkedIn" style={{color: 'grey'}}>
+                                                <LinkedIn fontSize="small"/>
+                                            </a>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                                <Grid item xs={12} style={{'paddingTop': '10px'}}>
+                                    <Typography variant="body2" style={{color: grey, fontFamily: font}}>
+                                    Ashish has over 10 years of experience in Investment Banking. He has previously worked with Morgan Stanley in Mumbai, New York and London and with Lincoln International and Aarayaa Advisory in Mumbai. He has been actively covering Chemicals, Packaging, Plastic and Metals and Mining sectors. Ashish holds a Bachelor’s degree in Commerce from the NarseeMonjee College of Commerce and Economics, Mumbai. He is also a Chartered Accountant.
+                                    </Typography>
+                                </Grid>
+                            </Hidden>
+
+                            <Hidden smDown>
+                                <Grid item xs={5}>
+                                    <img id="image-modal"src="/assets/saurabhs.png" alt="Saurabh Singhi" style={{width: '90%', height: 'auto'}}></img>
+                                </Grid>
+                                <Grid item md={6} >
+                                    <Grid container direction="column">
+                                        <Grid container spacing={0} direction="row">
+                                            <Grid item width="90%">
+                                                <Grid container direction="column">
+                                                    <Typography variant="h6" style={styleHeadline}>
+                                                    Ashish Chovatia
+                                                    </Typography>
+                                                    <Typography variant="subtitle1">
+                                                    
+                                                    </Typography>
+                                                </Grid>
+                                            </Grid>
+                                            <Grid item width="10%" style={{'paddingTop': '5px', 'paddingLeft': '5px'}}>
+                                                <a href="https://www.linkedin.com/in/ashish-chovatia-2038772/" target="_blank"  rel="noopener noreferrer" title="LinkedIn" style={{color: 'grey'}}>
+                                                    <LinkedIn fontSize="small"/>
+                                                </a>
+                                            </Grid>
+                                        </Grid>
+                                        <Grid item style={{'paddingTop': '10px'}}>
+                                            <Typography variant="body2" style={{color: grey, fontFamily: font, textJustify: 'inter word', textAlign: 'justify'}}>
+                                            Ashish has over 10 years of experience in Investment Banking. He has previously worked with Morgan Stanley in Mumbai, New York and London and with Lincoln International and Aarayaa Advisory in Mumbai. He has been actively covering Chemicals, Packaging, Plastic and Metals and Mining sectors. Ashish holds a Bachelor’s degree in Commerce from the NarseeMonjee College of Commerce and Economics, Mumbai. He is also a Chartered Accountant.
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Hidden>                            
                             <Grid item xs={1}/>
                         </Grid>
                     </div>
